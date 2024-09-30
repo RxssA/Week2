904cb967e8624c2994dc4c84b2aaf80b1e951057
G00401655@atu.ie <G00401655@atu.ie>
2024-09-30 17:37:33 +0100

    LAB2

--- src/main/java/ie/atu/week2_refresher/ProductService.java
+++ src/main/java/ie/atu/week2_refresher/ProductService.java
@@ -1,10 +1,12 @@
 package ie.atu.week2_refresher;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 @Service
+@FeignClient(value = "Micro2", url = "http://localhost:8081")
 public class ProductService {
 
     private List<Product> productList = new ArrayList<Product>();
--- src/main/java/ie/atu/week2_refresher/Week2RefresherApplication.java
+++ src/main/java/ie/atu/week2_refresher/Week2RefresherApplication.java
@@ -2,8 +2,11 @@ package ie.atu.week2_refresher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class Week2RefresherApplication {
 
     public static void main(String[] args) {
